63a611bdf82b0fbc7e43878260906939cf10aba4
kshah93 <kshah@lwolf.com>
2020-02-04 14:31:06 -0600

    adding files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err) 
+    );
+
+    return <div>Hello there</div>;
+};
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
